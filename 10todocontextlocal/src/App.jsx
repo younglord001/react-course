163ab115358edffe90b8babbfa0abfe804d9152e
youngloard001 <riteshmehrotra95@gmail.com>
2025-03-12 14:35:33 +0530

    todo Project Basic Setup Done

---
+++ 10todocontextlocal/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
